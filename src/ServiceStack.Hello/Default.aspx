19cef7b67c6af582ac1d5c7fbbc1cec9c5a26a76
Demis Bellot <demis.bellot@gmail.com>
2010-12-11 06:12:42 +0000

    added the most basic, self-contained ServiceStack web service

---
+++ src/ServiceStack.Hello/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ServiceStack.Hello._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>ServiceStack.Hello :: The smallest ServiceStack Web Service</title>
+</head>
+<body>
+    <h1>Anatomy of a ServiceStack Web Service</h1>
+    
+    <p>
+        To help
+    </p>
+    
+</body>
+</html>
---
+++ src/ServiceStack.Hello/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Funq;
+using ServiceStack.ServiceHost;
+using ServiceStack.WebHost.Endpoints;
+
+namespace ServiceStack.Hello
+{
+	[DataContract]
+	[RestService("/hello/{Name}")]
+	public class Hello
+	{
+		[DataMember]
+		public string Name { get; set; }
+	}
+
+	[DataContract]
+	public class HelloResponse
+	{
+		[DataMember]
+		public string Result { get; set; }
+	}
+
+	public class HelloService : IService<Hello>
+	{
+		public object Execute(Hello request)
+		{
+			return new HelloResponse { Result = "Hello, " + request.Name };
+		}
+	}
+
+	public class Global : System.Web.HttpApplication
+	{
+		public class HelloAppHost : AppHostBase
+		{
+			public HelloAppHost() : base("Hello Web Services", typeof(HelloService).Assembly) { }
+			public override void Configure(Container container) { }
+		}
+
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			var appHost = new HelloAppHost();
+			appHost.Init();
+		}
+	}
+}
