5bea936f4380a0c3d9313a2fb40c3f46c43a42db
Demis Bellot <demis.bellot@gmail.com>
2011-01-20 23:02:00 +0000

    checked in latest libs and pruned Global.asax.cs

--- src/ServiceStack.MovieRest/Global.asax.cs
+++ src/ServiceStack.MovieRest/Global.asax.cs
@@ -20,8 +20,8 @@ namespace ServiceStack.MovieRest
 					"~/App_Data/db.sqlite".MapHostAbsolutePath(),
 					SqliteOrmLiteDialectProvider.Instance));
 
-			var moviesService = container.Resolve<ResetMoviesService>();
-			moviesService.Post(null);
+			var resetMovies = container.Resolve<ResetMoviesService>();
+			resetMovies.Post(null);
 		}
 	}
 
@@ -30,36 +30,6 @@ namespace ServiceStack.MovieRest
 		protected void Application_Start(object sender, EventArgs e)
 		{
 			new AppHost().Init();
-		}
-
-		protected void Session_Start(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Application_BeginRequest(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Application_AuthenticateRequest(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Application_Error(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Session_End(object sender, EventArgs e)
-		{
-
-		}
-
-		protected void Application_End(object sender, EventArgs e)
-		{
-
 		}
 	}
 }
