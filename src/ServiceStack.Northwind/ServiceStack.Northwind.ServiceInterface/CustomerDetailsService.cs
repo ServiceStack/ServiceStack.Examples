675484e69ed1cf3d34612c30eecddb43171def16
Demis Bellot <demis.bellot@gmail.com>
2011-02-15 08:45:09 +0000

    Change CustomerDetails do use more succing db access

--- src/ServiceStack.Northwind/ServiceStack.Northwind.ServiceInterface/CustomerDetailsService.cs
+++ src/ServiceStack.Northwind/ServiceStack.Northwind.ServiceInterface/CustomerDetailsService.cs
@@ -14,23 +14,19 @@ namespace ServiceStack.Northwind.ServiceInterface
 
 		public override object OnGet(CustomerDetails request)
 		{
-			using (var dbConn = DbFactory.OpenDbConnection())
-			using (var dbCmd = dbConn.CreateCommand())
+			var customer = DbFactory.Exec(dbCmd => dbCmd.GetByIdOrDefault<Customer>(request.Id));
+			if (customer == null)
+				throw new HttpError(HttpStatusCode.NotFound,
+					new ArgumentException("Customer does not exist: " + request.Id));
+
+			var ordersService = base.ResolveService<OrdersService>();
+			var ordersResponse = (OrdersResponse)ordersService.Get(new Orders { CustomerId = customer.Id });
+
+			return new CustomerDetailsResponse
 			{
-				var customer = dbCmd.GetByIdOrDefault<Customer>(request.Id);
-				if (customer == null)
-					throw new HttpError(HttpStatusCode.NotFound,
-						new ArgumentException("Customer does not exist: " + request.Id));
-
-				var ordersService = base.ResolveService<OrdersService>();
-				var ordersResponse = (OrdersResponse)ordersService.Get(new Orders { CustomerId = customer.Id });
-
-				return new CustomerDetailsResponse
-				{
-					Customer = customer,
-					CustomerOrders = ordersResponse.Results,
-				};
-			}
+				Customer = customer,
+				CustomerOrders = ordersResponse.Results,
+			};
 		}
 	}
 }
