48e7109131737d5b6b856745455d5081c9ee8d65
Demis Bellot <demis.bellot@gmail.com>
2011-03-22 03:12:12 +0000

    annotate TODO service

--- src/Backbone.Todos/Global.asax.cs
+++ src/Backbone.Todos/Global.asax.cs
@@ -7,9 +7,10 @@ using ServiceStack.ServiceHost;
 
 namespace Backbone.Todos
 {
+	//Register REST Paths
 	[RestService("/todos")]
 	[RestService("/todos/{Id}")]
-	public class Todo
+	public class Todo //REST Resource Model
 	{
 		public long Id { get; set; }
 
@@ -20,21 +21,27 @@ namespace Backbone.Todos
 		public bool Done { get; set; }
 	}
 
+	//Implement TODO Rest Service
 	public class TodoService : RestServiceBase<Todo>
 	{
+		//Injected by IOC
 		public IRedisClientsManager RedisManager { get; set; }
 
 		public override object OnGet(Todo request)
 		{
+			//return all todos
 			if (request.Id == default(long)) 
 				return RedisManager.ExecAs<Todo>(r => r.GetAll());
 
+			//return single todo
 			return  RedisManager.ExecAs<Todo>(r => r.GetById(request.Id));
 		}
 
+		//Handles creaing a new and updating existing todo
 		public override object OnPost(Todo todo)
 		{
 			RedisManager.ExecAs<Todo>(r => {
+				//Get next id for new todo
 				if (todo.Id == default(long)) todo.Id = r.GetNextSequence();
 				r.Store(todo);
 			});
@@ -49,12 +56,15 @@ namespace Backbone.Todos
 		}
 	}
 
+	//Configure ServiceStack.NET web service
 	public class AppHost : AppHostBase
 	{
+		//Tell ServiceStack name and where to find your web services
 		public AppHost() : base("Backbone.js TODO", typeof(TodoService).Assembly) { }
 
 		public override void Configure(Container container)
 		{
+			//Register Redis factory in Funq IOC
 			container.Register<IRedisClientsManager>(new BasicRedisClientManager("localhost:6379"));
 		}
 	}
@@ -63,6 +73,7 @@ namespace Backbone.Todos
 	{
 		protected void Application_Start(object sender, EventArgs e)
 		{
+			//Start ServiceStack App
 			new AppHost().Init();
 		}
 	}
