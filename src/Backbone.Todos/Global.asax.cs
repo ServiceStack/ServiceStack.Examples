074c4477e49b67b328a91db7d03b926ae7a90a4e
Demis Bellot <demis.bellot@gmail.com>
2011-03-22 03:05:34 +0000

    Added ServiceStack's first Backbone.js application.
    Added the TODO app with a Redis backend.

---
+++ src/Backbone.Todos/Global.asax.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Funq;
+using ServiceStack.Redis;
+using ServiceStack.ServiceInterface;
+using ServiceStack.WebHost.Endpoints;
+using ServiceStack.ServiceHost;
+
+namespace Backbone.Todos
+{
+	[RestService("/todos")]
+	[RestService("/todos/{Id}")]
+	public class Todo
+	{
+		public long Id { get; set; }
+
+		public string Content { get; set; }
+
+		public int Order { get; set; }
+
+		public bool Done { get; set; }
+	}
+
+	public class TodoService : RestServiceBase<Todo>
+	{
+		public IRedisClientsManager RedisManager { get; set; }
+
+		public override object OnGet(Todo request)
+		{
+			if (request.Id == default(long)) 
+				return RedisManager.ExecAs<Todo>(r => r.GetAll());
+
+			return  RedisManager.ExecAs<Todo>(r => r.GetById(request.Id));
+		}
+
+		public override object OnPost(Todo todo)
+		{
+			RedisManager.ExecAs<Todo>(r => {
+				if (todo.Id == default(long)) todo.Id = r.GetNextSequence();
+				r.Store(todo);
+			});
+
+			return todo;
+		}
+
+		public override object OnDelete(Todo request)
+		{
+			RedisManager.ExecAs<Todo>(r => r.DeleteById(request.Id));
+			return null;
+		}
+	}
+
+	public class AppHost : AppHostBase
+	{
+		public AppHost() : base("Backbone.js TODO", typeof(TodoService).Assembly) { }
+
+		public override void Configure(Container container)
+		{
+			container.Register<IRedisClientsManager>(new BasicRedisClientManager("localhost:6379"));
+		}
+	}
+
+	public class Global : System.Web.HttpApplication
+	{
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			new AppHost().Init();
+		}
+	}
+}
