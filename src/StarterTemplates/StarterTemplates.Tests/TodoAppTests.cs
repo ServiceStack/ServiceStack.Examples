d4423df3414aea1e55bbf40881395f15694f52fc
Demis Bellot <demis.bellot@gmail.com>
2011-03-25 13:22:33 +0000

    Added C# Rest client tests for the TODO app

---
+++ src/StarterTemplates/StarterTemplates.Tests/TodoAppTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using ServiceStack.ServiceClient.Web;
+using System.Collections.Generic;
+using StarterTemplates.Common;
+
+namespace StarterTemplates.Tests
+{
+	[TestFixture]
+	public class TodoAppTests
+	{
+		[Test]
+		public void Can_view_create_and_delete_TODOs_over_REST()
+		{
+			var restClient = new JsonServiceClient("http://localhost/RootPath35");
+
+			var all = restClient.Get<List<Todo>>("/todos");
+			Assert.That(all.Count, Is.EqualTo(0));
+
+			var todo = restClient.Post<Todo>("/todos", new Todo { Content = "New TODO", Order = 1 });
+			Assert.That(todo.Id, Is.GreaterThan(0));
+			Assert.That(todo.Content, Is.EqualTo("New TODO"));
+
+			all = restClient.Get<List<Todo>>("/todos");
+			Assert.That(all.Count, Is.EqualTo(1));
+
+			todo.Content = "Updated TODO";
+			todo = restClient.Post<Todo>("/todos", todo);
+			Assert.That(todo.Content, Is.EqualTo("Updated TODO"));
+
+			restClient.Delete<Todo>("/todos/" + todo.Id);
+
+			all = restClient.Get<List<Todo>>("/todos");
+			Assert.That(all.Count, Is.EqualTo(0));
+		}
+	}
+}
