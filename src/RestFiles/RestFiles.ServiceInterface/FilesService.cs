cef545c2d942316f6190138b207495d104fe8b1c
Demis Bellot <demis.bellot@gmail.com>
2011-12-26 23:45:27 -0500

    re-factor RestFiles to make it look more like @AlexZeitler's WCF Web APIs port.

--- src/RestFiles/RestFiles.ServiceInterface/FilesService.cs
+++ src/RestFiles/RestFiles.ServiceInterface/FilesService.cs
@@ -1,28 +1,22 @@
 using System;
 using System.IO;
 using System.Net;
+using RestFiles.ServiceInterface.Support;
 using RestFiles.ServiceModel.Operations;
 using RestFiles.ServiceModel.Types;
 using ServiceStack.Common.Web;
 using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
+using Dto = RestFiles.ServiceModel.Types;
 using File = System.IO.File;
-using FileResult = RestFiles.ServiceModel.Types.FileResult;
-
-/* For syntax highlighting and better readability of this file, view it on GitHub:
- * https://github.com/ServiceStack/ServiceStack.Examples/blob/master/src/RestFiles/RestFiles.Tests/SyncRestClientTests.cs
- */
 
 namespace RestFiles.ServiceInterface
 {
 	/// <summary>
-	///  Contains the entire implementation of ServiceStack's REST /files Web Service:
-	/// 
-	///  Complete file management 
-	/// 
+	///  Contains the entire file manager implementation of ServiceStack's REST /files Web Service:
+	///  Demo: http://www.servicestack.net/RestFiles/
 	/// </summary>
-	public class FilesService
-		: RestServiceBase<Files>
+	public class FilesService : RestServiceBase<Files>
 	{
 		public AppConfig Config { get; set; }
 
@@ -33,7 +27,7 @@ namespace RestFiles.ServiceInterface
 			var isDirectory = Directory.Exists(targetFile.FullName);
 
 			if (!isDirectory && request.ForDownload)
-				return new HttpResult(targetFile, true);
+				return new HttpResult(targetFile, asAttachment:true);
 
 			var response = isDirectory
 				? new FilesResponse { Directory = GetFolderResult(targetFile.FullName) }
@@ -54,12 +48,9 @@ namespace RestFiles.ServiceInterface
 				"POST only supports uploading new files. Use PUT to replace contents of an existing file");
 
 			if (!Directory.Exists(targetDir.FullName))
-			{
 				Directory.CreateDirectory(targetDir.FullName);
-			}
 
-			foreach (var uploadedFile in base.RequestContext.Files)
-			{
+			foreach (var uploadedFile in base.RequestContext.Files) {
 				var newFilePath = Path.Combine(targetDir.FullName, uploadedFile.FileName);
 				uploadedFile.SaveTo(newFilePath);
 			}
@@ -73,7 +64,7 @@ namespace RestFiles.ServiceInterface
 
 			if (!this.Config.TextFileExtensions.Contains(targetFile.Extension))
 				throw new NotSupportedException("PUT Can only update text files, not: " + targetFile.Extension);
-
+			
 			if (request.TextContents == null)
 				throw new ArgumentNullException("TextContents");
 
@@ -85,36 +76,31 @@ namespace RestFiles.ServiceInterface
 		public override object OnDelete(Files request)
 		{
 			var targetFile = GetAndValidateExistingPath(request);
-			
-			File.Delete(targetFile.FullName);
 
+			File.Delete(targetFile.FullName);
+			
 			return new FilesResponse();
 		}
 
-		private FolderResult GetFolderResult(string targetPath)
-		{
+		private FolderResult GetFolderResult(string targetPath) {
 			var result = new FolderResult();
 
-			foreach (var dirPath in Directory.GetDirectories(targetPath))
-			{
+			foreach (var dirPath in Directory.GetDirectories(targetPath)) {
 				var dirInfo = new DirectoryInfo(dirPath);
 
 				if (this.Config.ExcludeDirectories.Contains(dirInfo.Name)) continue;
-
-				result.Folders.Add(new Folder
-				{
+				
+				result.Folders.Add(new Folder {
 					Name = dirInfo.Name,
 					ModifiedDate = dirInfo.LastWriteTimeUtc,
 					FileCount = dirInfo.GetFiles().Length
 				});
 			}
 
-			foreach (var filePath in Directory.GetFiles(targetPath))
-			{
+			foreach (var filePath in Directory.GetFiles(targetPath)) {
 				var fileInfo = new FileInfo(filePath);
-
-				result.Files.Add(new ServiceModel.Types.File
-				{
+				
+				result.Files.Add(new ServiceModel.Types.File {
 					Name = fileInfo.Name,
 					Extension = fileInfo.Extension,
 					FileSizeBytes = fileInfo.Length,
@@ -126,30 +112,22 @@ namespace RestFiles.ServiceInterface
 			return result;
 		}
 
-		private FileInfo GetPath(Files request)
-		{
-			var targetPath = Path.Combine(this.Config.RootDirectory,
-				GetSafePath(request.Path ?? string.Empty));
-
-			return new FileInfo(targetPath);
+		private FileInfo GetPath(Files request) {
+			return new FileInfo(Path.Combine(this.Config.RootDirectory, request.Path.GetSafePath()));
 		}
 
-		private FileInfo GetAndValidateExistingPath(Files request)
-		{
+		private FileInfo GetAndValidateExistingPath(Files request) {
 			var targetFile = GetPath(request);
 			if (!targetFile.Exists && !Directory.Exists(targetFile.FullName))
-				throw new HttpError(HttpStatusCode.NotFound,
-					new FileNotFoundException("Could not find: " + request.Path));
+				throw new HttpError(HttpStatusCode.NotFound, new FileNotFoundException("Could not find: " + request.Path));
 
 			return targetFile;
 		}
 
-		private FileResult GetFileResult(FileInfo fileInfo)
-		{
+		private Dto.FileResult GetFileResult(FileInfo fileInfo) {
 			var isTextFile = this.Config.TextFileExtensions.Contains(fileInfo.Extension);
 
-			return new FileResult
-			{
+			return new Dto.FileResult {
 				Name = fileInfo.Name,
 				Extension = fileInfo.Extension,
 				FileSizeBytes = fileInfo.Length,
@@ -157,21 +135,6 @@ namespace RestFiles.ServiceInterface
 				Contents = isTextFile ? File.ReadAllText(fileInfo.FullName) : null,
 				ModifiedDate = fileInfo.LastWriteTimeUtc,
 			};
-		}
-
-		public static string GetSafePath(string filePath)
-		{
-			//Strip invalid chars
-			foreach (var invalidChar in Path.GetInvalidPathChars())
-			{
-				filePath = filePath.Replace(invalidChar.ToString(), string.Empty);
-			}
-
-			return filePath
-			.TrimStart('.', '/', '\\')					//Remove illegal chars at the start
-			.Replace('\\', '/')							//Switch all to use the same seperator
-			.Replace("../", string.Empty)				//Remove access to top-level directories anywhere else 
-			.Replace('/', Path.DirectorySeparatorChar); //Switch all to use the OS seperator
 		}
 	}
 }
---
+++ src/RestFiles/RestFiles.ServiceInterface/Support/FileExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace RestFiles.ServiceInterface.Support
+{
+	public static class FileExtensions
+	{
+		public static string GetSafePath(this string filePath)
+		{
+			if (string.IsNullOrEmpty(filePath)) return string.Empty;
+
+			//Strip invalid chars
+			foreach (var invalidChar in Path.GetInvalidPathChars())
+			{
+				filePath = filePath.Replace(invalidChar.ToString(), String.Empty);
+			}
+
+			return filePath
+			.TrimStart('.', '/', '\\')					//Remove illegal chars at the start
+			.Replace('\\', '/')							//Switch all to use the same seperator
+			.Replace("../", String.Empty)				//Remove access to top-level directories anywhere else 
+			.Replace('/', Path.DirectorySeparatorChar); //Switch all to use the OS seperator
+		}
+	}
+}
