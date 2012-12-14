fe0d80c55e33ad8f0b58f15bd9f38ffdffbfb4a0
Demis Bellot <demis.bellot@gmail.com>
2012-12-14 03:44:08 -0500

    Updated RestIntro into new API and latest NuGets

--- src/RestIntro/RestIntro.ServiceInterface/CustomerService.cs
+++ src/RestIntro/RestIntro.ServiceInterface/CustomerService.cs
@@ -1,57 +1,42 @@
-using System.Net;
 using RestIntro.ServiceModel;
+using ServiceStack.Common;
 using ServiceStack.Common.Web;
 using ServiceStack.OrmLite;
 using ServiceStack.ServiceInterface;
-using ServiceStack.Text;
 
 namespace RestIntro.ServiceInterface
 {
     /// <summary>
     /// Create your ServiceStack rest-ful web service implementation. 
     /// </summary>
-    public class CustomerService : RestServiceBase<Customer>
+    public class CustomerService : Service
     {
-        /// <summary>
-        /// Gets or sets the database factory. The built-in IoC used with ServiceStack autowires this property.
-        /// </summary>
-        public IDbConnectionFactory DbFactory { get; set; }
-
-        public override object OnGet(Customer request)
+        public object Get(Customer request)
         {
             if (request.Id != default(long))
-                return DbFactory.Run(dbCmd => dbCmd.GetById<Customer>(request.Id));
+                return Db.GetById<Customer>(request.Id);
 
-            return DbFactory.Run(dbCmd => dbCmd.Select<Customer>());
+            return Db.Select<Customer>();
         }
 
-        public override object OnPost(Customer customer)
+        public object Post(Customer customer)
         {
-            DbFactory.Run(dbCmd =>
-            {
-                dbCmd.Save(customer);
-                customer.Id = (int)dbCmd.GetLastInsertId();
-            });
+            Db.Save(customer);
+            customer.Id = (int)Db.GetLastInsertId();
 
-            var pathToNewResource = base.RequestContext.AbsoluteUri.WithTrailingSlash() + customer.Id;
-            return new HttpResult(customer) {
-                StatusCode = HttpStatusCode.Created,
-                Headers = {
-                    { HttpHeaders.Location, pathToNewResource },
-                }
-            };
+            var pathToNewResource = base.RequestContext.AbsoluteUri.CombineWith(customer.Id.ToString());
+            return HttpResult.Status201Created(customer, pathToNewResource);
         }
 
-        public override object OnPut(Customer customer)
+        public Customer Put(Customer customer)
         {
-            DbFactory.Run(dbCmd => dbCmd.Save(customer));
+            Db.Save(customer);
             return customer;
         }
 
-        public override object OnDelete(Customer request)
+        public void Delete(Customer request)
         {
-            DbFactory.Run(dbCmd => dbCmd.DeleteById<Customer>(request.Id));
-            return null;
+            Db.DeleteById<Customer>(request.Id);
         }
     }
 }
