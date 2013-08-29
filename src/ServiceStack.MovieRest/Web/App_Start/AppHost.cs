8bdaa98f0f69d38a29299e7fd687e22af02f9c81
ChrisMcKee <pcdevils@gmail.com>
2013-08-29 23:52:07 +0100

    Changed page to match standard js JSON format.

--- src/ServiceStack.MovieRest/Web/App_Start/AppHost.cs
+++ src/ServiceStack.MovieRest/Web/App_Start/AppHost.cs
@@ -1,33 +1,21 @@
-using System;
-using System.Linq;
-using System.Configuration;
-using System.Collections.Generic;
-using ServiceStack.Configuration;
-using ServiceStack.OrmLite;
-using ServiceStack.ServiceInterface;
-using ServiceStack.ServiceInterface.Auth;
-using ServiceStack.ServiceInterface.ServiceModel;
-using ServiceStack.WebHost.Endpoints;
+using ServiceStack.MovieRest.App_Start;
+using WebActivator;
 
-[assembly: WebActivator.PreApplicationStartMethod(typeof(ServiceStack.MovieRest.App_Start.MovieAppHost), "Start")]
-
-
-/**
- * Entire ServiceStack Starter Template configured with a 'Hello' Web Service and a 'Todo' Rest Service.
- *
- * Auto-Generated Metadata API page at: /metadata
- * See other complete web service examples at: https://github.com/ServiceStack/ServiceStack.Examples
- */
+[assembly: PreApplicationStartMethod(typeof (MovieAppHost), "Start")]
 
 namespace ServiceStack.MovieRest.App_Start
 {
+	using Funq;
 	using ServiceStack.Common.Utils;
+	using ServiceStack.OrmLite;
 	using ServiceStack.OrmLite.Sqlite;
 	using ServiceStack.ServiceInterface.Cors;
+	using ServiceStack.Text;
+	using ServiceStack.WebHost.Endpoints;
 
 	public class MovieAppHost
 		: AppHostBase
-	{		
+	{
 		/// <summary>
 		///     Initializes a new instance of your ServiceStack application, with the specified name and assembly containing the services.
 		/// </summary>
@@ -35,12 +23,15 @@ namespace ServiceStack.MovieRest.App_Start
 		{
 		}
 
-		public override void Configure(Funq.Container container)
+		public override void Configure(Container container)
 		{
+			JsConfig.DateHandler = JsonDateHandler.ISO8601;
+
 			//Set JSON web services to return idiomatic JSON camelCase properties
-			ServiceStack.Text.JsConfig.EmitCamelCaseNames = true;
-			
-			container.Register<IDbConnectionFactory>(c => new OrmLiteConnectionFactory("~/App_Data/db.sqlite".MapHostAbsolutePath(), SqliteOrmLiteDialectProvider.Instance));
+			JsConfig.EmitCamelCaseNames = true;
+
+			container.Register<IDbConnectionFactory>(
+				c => new OrmLiteConnectionFactory("~/App_Data/db.sqlite".MapHostAbsolutePath(), SqliteOrmLiteDialectProvider.Instance));
 
 			using (var resetMovies = container.Resolve<ResetMoviesService>())
 			{
@@ -50,12 +41,11 @@ namespace ServiceStack.MovieRest.App_Start
 			Plugins.Add(new CorsFeature()); //Enable CORS
 
 			SetConfig(new EndpointHostConfig
-			{
-				DebugMode = true //Show StackTraces for easier debugging (default auto inferred by Debug/Release builds)
-			});
+				          {
+					          DebugMode = true //Show StackTraces for easier debugging (default auto inferred by Debug/Release builds)
+				          });
 		}
 
-		
 
 		public static void Start()
 		{
