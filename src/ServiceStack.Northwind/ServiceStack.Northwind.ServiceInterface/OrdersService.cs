3293776a3ea789985438a671160698d27ddbdf17
Demis Bellot <demis.bellot@gmail.com>
2013-09-11 12:46:33 -0400

    Update OrdersService.cs

--- src/ServiceStack.Northwind/ServiceStack.Northwind.ServiceInterface/OrdersService.cs
+++ src/ServiceStack.Northwind/ServiceStack.Northwind.ServiceInterface/OrdersService.cs
@@ -1,51 +1,39 @@
-﻿namespace ServiceStack.Northwind.ServiceInterface
+using System.Collections.Generic;
+using System.Linq;
+using ServiceStack.Common.Extensions;
+using ServiceStack.Northwind.ServiceModel.Operations;
+using ServiceStack.Northwind.ServiceModel.Types;
+using ServiceStack.OrmLite;
+using ServiceStack.ServiceInterface;
+
+namespace ServiceStack.Northwind.ServiceInterface
 {
-	using System.Collections.Generic;
-	using System.Linq;
-	using ServiceStack.Common.Extensions;
-	using ServiceStack.Northwind.ServiceModel.Operations;
-	using ServiceStack.Northwind.ServiceModel.Types;
-	using ServiceStack.OrmLite;
-	using ServiceStack.ServiceInterface;
+    public class OrdersService : Service
+    {
+        private const int PageCount = 20;
 
-	public class OrdersService : Service
-	{
-		private const int PageCount = 20;
+        public object Get(Orders request)
+        {
+            var orders = request.CustomerId.IsNullOrEmpty()
+                ? Db.Select<Order>(order => order.OrderByDescending(o => o.OrderDate))
+                      .Skip((request.Page.GetValueOrDefault(1) - 1)*PageCount)
+                      .Take(PageCount)
+                      .ToList()
+                : Db.Select<Order>(order => order.Where(o => o.CustomerId == request.CustomerId));
 
-		public IDbConnectionFactory DbFactory { get; set; }
+            if (orders.Count == 0)
+                return new OrdersResponse();
 
-		public object Get(Orders request)
-		{
-			using (var dbConn = DbFactory.OpenDbConnection())
-			using (var dbCmd = dbConn.CreateCommand())
-			{
-				List<Order> orders;
+            var orderDetails = Db.Select<OrderDetail>(detail => Sql.In(detail.OrderId, orders.ConvertAll(x => x.Id)));
 
-				if (request.CustomerId.IsNullOrEmpty())
-				{
-					orders = dbCmd.Select<Order>(order => order.OrderByDescending(o => o.OrderDate))
-					              .Skip((request.Page.GetValueOrDefault(1) - 1)*PageCount)
-					              .Take(PageCount)
-					              .ToList();
-				}
-				else orders = dbCmd.Select<Order>(order => order.Where(o => o.CustomerId == request.CustomerId));
+            var orderDetailsLookup = orderDetails.ToLookup(o => o.OrderId);
 
-				if (orders.Count == 0)
-					return new OrdersResponse();
+            var customerOrders = orders.ConvertAll(o => new CustomerOrder {
+                Order = o,
+                OrderDetails = orderDetailsLookup[o.Id].ToList()
+            });
 
-				var orderDetails = dbCmd.Select<OrderDetail>(detail => Sql.In(detail.OrderId, orders.ConvertAll(x => x.Id)));
-
-				var orderDetailsLookup = orderDetails.ToLookup(o => o.OrderId);
-
-				var customerOrders = orders.ConvertAll(o =>
-				                                       new CustomerOrder
-					                                       {
-						                                       Order = o,
-						                                       OrderDetails = orderDetailsLookup[o.Id].ToList()
-					                                       });
-
-				return new OrdersResponse {Results = customerOrders};
-			}
-		}
-	}
+            return new OrdersResponse { Results = customerOrders };
+        }
+    }
 }
