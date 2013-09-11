2fd326fd586031854939d4c184bb8a27c30b6acb
Demis Bellot <demis.bellot@gmail.com>
2013-09-11 12:43:15 -0400

    Update CustomerDetailsService.cs

--- src/ServiceStack.Northwind/ServiceStack.Northwind.ServiceInterface/CustomerDetailsService.cs
+++ src/ServiceStack.Northwind/ServiceStack.Northwind.ServiceInterface/CustomerDetailsService.cs
@@ -1,32 +1,30 @@
-﻿namespace ServiceStack.Northwind.ServiceInterface
+using System;
+using System.Net;
+using ServiceStack.Common.Web;
+using ServiceStack.Northwind.ServiceModel.Operations;
+using ServiceStack.Northwind.ServiceModel.Types;
+using ServiceStack.OrmLite;
+using ServiceStack.ServiceInterface;
+
+namespace ServiceStack.Northwind.ServiceInterface
 {
-	using System;
-	using System.Net;
-	using ServiceStack.Common.Web;
-	using ServiceStack.Northwind.ServiceModel.Operations;
-	using ServiceStack.Northwind.ServiceModel.Types;
-	using ServiceStack.OrmLite;
-	using ServiceStack.ServiceInterface;
+    public class CustomerDetailsService : Service
+    {
+        public CustomerDetailsResponse Get(CustomerDetails request)
+        {
+            var customer = Db.GetByIdOrDefault<Customer>(request.Id);
+            if (customer == null)
+                throw new HttpError(HttpStatusCode.NotFound,
+                                    new ArgumentException("Customer does not exist: " + request.Id));
 
-	public class CustomerDetailsService : Service
-	{
-		public IDbConnectionFactory DbFactory { get; set; }
+            var ordersService = base.ResolveService<OrdersService>();
+            var ordersResponse = (OrdersResponse) ordersService.Get(new Orders {CustomerId = customer.Id});
 
-		public CustomerDetailsResponse Get(CustomerDetails request)
-		{
-			var customer = DbFactory.Run(dbCmd => dbCmd.GetByIdOrDefault<Customer>(request.Id));
-			if (customer == null)
-				throw new HttpError(HttpStatusCode.NotFound,
-				                    new ArgumentException("Customer does not exist: " + request.Id));
-
-			var ordersService = base.ResolveService<OrdersService>();
-			var ordersResponse = (OrdersResponse) ordersService.Get(new Orders {CustomerId = customer.Id});
-
-			return new CustomerDetailsResponse
-				       {
-					       Customer = customer,
-					       CustomerOrders = ordersResponse.Results,
-				       };
-		}
-	}
+            return new CustomerDetailsResponse
+            {
+                Customer = customer,
+                CustomerOrders = ordersResponse.Results,
+            };
+        }
+    }
 }
