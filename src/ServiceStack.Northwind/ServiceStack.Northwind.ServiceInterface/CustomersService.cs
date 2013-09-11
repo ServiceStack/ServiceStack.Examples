f74ca2479ed78cbfef9dddd5f3edc26c593ebe9c
Demis Bellot <demis.bellot@gmail.com>
2013-09-11 12:43:55 -0400

    Update CustomersService.cs

--- src/ServiceStack.Northwind/ServiceStack.Northwind.ServiceInterface/CustomersService.cs
+++ src/ServiceStack.Northwind/ServiceStack.Northwind.ServiceInterface/CustomersService.cs
@@ -1,15 +1,15 @@
-﻿namespace ServiceStack.Northwind.ServiceInterface
-{
-	using ServiceStack.Northwind.ServiceModel.Operations;
-	using ServiceStack.Northwind.ServiceModel.Types;
-	using ServiceStack.OrmLite;
-	using ServiceStack.ServiceInterface;
+using ServiceStack.Northwind.ServiceModel.Operations;
+using ServiceStack.Northwind.ServiceModel.Types;
+using ServiceStack.OrmLite;
+using ServiceStack.ServiceInterface;
 
-	public class CustomersService : Service
-	{
-		public CustomersResponse Get(Customers request)
-		{
-			return new CustomersResponse { Customers = Db.Select<Customer>() };
-		}
-	}
+namespace ServiceStack.Northwind.ServiceInterface
+{
+    public class CustomersService : Service
+    {
+        public CustomersResponse Get(Customers request)
+        {
+            return new CustomersResponse { Customers = Db.Select<Customer>() };
+        }
+    }
 }
