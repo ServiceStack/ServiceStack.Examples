9c2ccc952848800312d38cf24c5dbfdadf2e2e5a
Demis Bellot <demis.bellot@gmail.com>
2014-02-20 16:26:29 -0500

    Upgrade MovieRest to v4.0.11

--- src/ServiceStack.MovieRest/Web/App_Start/AppHost.cs
+++ src/ServiceStack.MovieRest/Web/App_Start/AppHost.cs
@@ -1,28 +1,22 @@
-using ServiceStack.MovieRest.App_Start;
 
 namespace ServiceStack.MovieRest.App_Start
 {
 	using Funq;
-	using ServiceStack.Common.Utils;
-	using ServiceStack.OrmLite;
-	using ServiceStack.OrmLite.Sqlite;
-	using ServiceStack.ServiceInterface.Cors;
-	using ServiceStack.Text;
-	using ServiceStack.WebHost.Endpoints;
+	using OrmLite;
+	using Text;
+    using Data;
 
-	public class AppHost
+    public class AppHost
 		: AppHostBase
 	{
 		/// <summary>
 		///     Initializes a new instance of your ServiceStack application, with the specified name and assembly containing the services.
 		/// </summary>
-		public AppHost() : base("ServiceStack REST at the Movies!", typeof (MovieService).Assembly)
-		{
-		}
+		public AppHost() : base("ServiceStack REST at the Movies!", typeof (MovieService).Assembly) {}
 
 		public override void Configure(Container container)
 		{
-			JsConfig.DateHandler = JsonDateHandler.ISO8601;
+			JsConfig.DateHandler = DateHandler.ISO8601;
 
 			//Set JSON web services to return idiomatic JSON camelCase properties
 			JsConfig.EmitCamelCaseNames = true;
@@ -37,10 +31,9 @@ namespace ServiceStack.MovieRest.App_Start
 
 			Plugins.Add(new CorsFeature()); //Enable CORS
 
-			SetConfig(new EndpointHostConfig
-				          {
-					          DebugMode = true //Show StackTraces for easier debugging (default auto inferred by Debug/Release builds)
-				          });
+			SetConfig(new HostConfig {
+				DebugMode = true //Show StackTraces for easier debugging (default auto inferred by Debug/Release builds)
+			});
 		}
 	}
 }
--- src/ServiceStack.MovieRest/Web/MovieService.cs
+++ src/ServiceStack.MovieRest/Web/MovieService.cs
@@ -1,177 +1,167 @@
+using System.Runtime.Serialization;
+
 namespace ServiceStack.MovieRest
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Net;
-	using ServiceStack.Common;
-	using ServiceStack.Common.Web;
-	using ServiceStack.DataAnnotations;
-	using ServiceStack.OrmLite;
-	using ServiceStack.ServiceHost;
-	using ServiceStack.ServiceInterface;
-	using ServiceStack.Text;
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using DataAnnotations;
+    using OrmLite;
 
-	/// <summary>
-	///     Define your ServiceStack web service request (i.e. Request DTO).
-	/// </summary>
-	/// <remarks>The route is defined here rather than in the AppHost.</remarks>
-	[Api("GET or DELETE a single movie by Id. Use POST to create a new Movie and PUT to update it")]
-	[Route("/movies", "POST,PUT,PATCH,DELETE")]
-	[Route("/movies/{Id}")]
-	public class Movie : IReturn<MovieResponse>
-	{
-		/// <summary>
-		///     Initializes a new instance of the movie.
-		/// </summary>
-		public Movie()
-		{
-			this.Genres = new List<string>();
-		}
+    /// <summary>
+    ///     Define your ServiceStack web service request (i.e. Request DTO).
+    /// </summary>
+    /// <remarks>The route is defined here rather than in the AppHost.</remarks>
+    [Api("GET or DELETE a single movie by Id. Use POST to create a new Movie and PUT to update it")]
+    [Route("/movies", "POST,PUT,PATCH,DELETE")]
+    [Route("/movies/{Id}")]
+    public class Movie : IReturn<MovieResponse>
+    {
+        /// <summary>
+        ///     Initializes a new instance of the movie.
+        /// </summary>
+        public Movie()
+        {
+            this.Genres = new List<string>();
+        }
 
-		/// <summary>
-		///     Gets or sets the id of the movie. The id will be automatically incremented when added.
-		/// </summary>
-		[AutoIncrement]
-		public int Id { get; set; }
+        /// <summary>
+        ///     Gets or sets the id of the movie. The id will be automatically incremented when added.
+        /// </summary>
+        [AutoIncrement]
+        public int Id { get; set; }
 
-		public string ImdbId { get; set; }
-		public string Title { get; set; }
-		public decimal Rating { get; set; }
-		public string Director { get; set; }
-		public DateTime ReleaseDate { get; set; }
-		public string TagLine { get; set; }
-		public List<string> Genres { get; set; }
-	}
+        public string ImdbId { get; set; }
+        public string Title { get; set; }
+        public decimal Rating { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string TagLine { get; set; }
+        public List<string> Genres { get; set; }
+    }
 
-	/// <summary>
-	///     Define your ServiceStack web service response (i.e. Response DTO).
-	/// </summary>
-	public class MovieResponse
-	{
-		/// <summary>
-		///     Gets or sets the movie.
-		/// </summary>
-		public Movie Movie { get; set; }
-	}
+    /// <summary>
+    ///     Define your ServiceStack web service response (i.e. Response DTO).
+    /// </summary>
+    public class MovieResponse
+    {
+        /// <summary>
+        ///     Gets or sets the movie.
+        /// </summary>
+        public Movie Movie { get; set; }
+    }
 
-	/// <summary>
-	///     Create your ServiceStack restful web service implementation.
-	/// </summary>
-	public class MovieService : Service
-	{
-		/// <summary>
-		///     GET /movies/{Id}
-		/// </summary>
-		public MovieResponse Get(Movie movie)
-		{
-			return new MovieResponse
-				       {
-					       Movie = Db.Id<Movie>(movie.Id),
-				       };
-		}
+    /// <summary>
+    ///     Define your ServiceStack web service request (i.e. Request DTO).
+    /// </summary>
+    /// <remarks>The route is defined here rather than in the AppHost.</remarks>
+    [Api("Find movies by genre, or all movies if no genre is provided")]
+    [Route("/movies", "GET, OPTIONS")]
+    [Route("/movies/genres/{Genre}")]
+    public class Movies : IReturn<MoviesResponse>
+    {
+        public string Genre { get; set; }
+    }
 
-		/// <summary>
-		///     POST /movies
-		///     returns HTTP Response =>
-		///     201 Created
-		///     Location: http://localhost/ServiceStack.MovieRest/movies/{newMovieId}
-		///     {newMovie DTO in [xml|json|jsv|etc]}
-		/// </summary>
-		public object Post(Movie movie)
-		{
-			Db.Insert(movie);
-			var newMovieId = Db.GetLastInsertId();
+    /// <summary>
+    ///     Define your ServiceStack web service response (i.e. Response DTO).
+    /// </summary>
+    [DataContract]
+    public class MoviesResponse
+    {
+        /// <summary>
+        ///     Gets or sets the list of movies.
+        /// </summary>
+        [DataMember]
+        public List<Movie> Movies { get; set; }
+    }
 
-			var newMovie = new MovieResponse
-				               {
-					               Movie = Db.Id<Movie>(newMovieId),
-				               };
+    /// <summary>
+    ///     Create your ServiceStack restful web service implementation.
+    /// </summary>
+    public class MovieService : Service
+    {
+        /// <summary>
+        ///     GET /movies
+        ///     GET /movies/genres/{Genre}
+        /// </summary>
+        public object Get(Movies request)
+        {
+            return new MoviesResponse {
+                Movies = request.Genre.IsNullOrEmpty()
+                    ? Db.Select<Movie>()
+                    : Db.Select<Movie>("Genres LIKE {0}", "%{0}%".Fmt(request.Genre))
+            };
+        }
 
-			return new HttpResult(newMovie)
-				       {
-					       StatusCode = HttpStatusCode.Created,
-					       Headers =
-						       {
-							       {HttpHeaders.Location, base.Request.AbsoluteUri.CombineWith(newMovieId.ToString())}
-						       }
-				       };			
-		}
+        /// <summary>
+        ///     GET /movies/{Id}
+        /// </summary>
+        public MovieResponse Get(Movie movie)
+        {
+            return new MovieResponse
+            {
+                Movie = Db.SingleById<Movie>(movie.Id),
+            };
+        }
 
-		/// <summary>
-		///     PUT /movies/{id}
-		/// </summary>
-		public object Put(Movie movie)
-		{
-			Db.Update(movie);
+        /// <summary>
+        ///     POST /movies
+        ///     returns HTTP Response =>
+        ///     201 Created
+        ///     Location: http://localhost/ServiceStack.MovieRest/movies/{newMovieId}
+        ///     {newMovie DTO in [xml|json|jsv|etc]}
+        /// </summary>
+        public object Post(Movie movie)
+        {
+            Db.Save(movie);
+            var newMovieId = movie.Id;
 
-			return new HttpResult
-				       {
-					       StatusCode = HttpStatusCode.NoContent,
-					       Headers =
-						       {
-							       {HttpHeaders.Location, this.RequestContext.AbsoluteUri.CombineWith(movie.Id.ToString())}
-						       }
-				       };
-		}
+            var newMovie = new MovieResponse
+            {
+                Movie = Db.SingleById<Movie>(newMovieId),
+            };
 
-		/// <summary>
-		///     DELETE /movies/{Id}
-		/// </summary>
-		public object Delete(Movie request)
-		{
-			Db.DeleteById<Movie>(request.Id);
+            return new HttpResult(newMovie)
+            {
+                StatusCode = HttpStatusCode.Created,
+                Headers = {
+					{HttpHeaders.Location, base.Request.AbsoluteUri.CombineWith(newMovieId.ToString())}
+				}
+            };
+        }
 
-			return new HttpResult
-				       {
-					       StatusCode = HttpStatusCode.NoContent,
-					       Headers =
-						       {
-							       {HttpHeaders.Location, this.RequestContext.AbsoluteUri.CombineWith(request.Id.ToString())}
-						       }
-				       };
-		}
-	}
+        /// <summary>
+        ///     PUT /movies/{id}
+        /// </summary>
+        public object Put(Movie movie)
+        {
+            Db.Update(movie);
 
-	/// <summary>
-	///     Define your ServiceStack web service request (i.e. Request DTO).
-	/// </summary>
-	/// <remarks>The route is defined here rather than in the AppHost.</remarks>
-	[Api("Find movies by genre, or all movies if no genre is provided")]
-	[Route("/movies", "GET, OPTIONS")]
-	[Route("/movies/genres/{Genre}")]
-	public class Movies
-	{
-		public string Genre { get; set; }
-	}
+            return new HttpResult
+            {
+                StatusCode = HttpStatusCode.NoContent,
+                Headers =
+					{
+						{HttpHeaders.Location, this.Request.AbsoluteUri.CombineWith(movie.Id.ToString())}
+					}
+            };
+        }
 
-	/// <summary>
-	///     Define your ServiceStack web service response (i.e. Response DTO).
-	/// </summary>
-	public class MoviesResponse
-	{
-		/// <summary>
-		///     Gets or sets the list of movies.
-		/// </summary>
-		public List<Movie> Movies { get; set; }
-	}
+        /// <summary>
+        ///     DELETE /movies/{Id}
+        /// </summary>
+        public object Delete(Movie request)
+        {
+            Db.DeleteById<Movie>(request.Id);
 
-	/// <summary>
-	///     Create your ServiceStack RESTful web service implementation.
-	/// </summary>
-	public class MoviesService : Service
-	{
-		/// <summary>
-		///     GET /movies
-		///     GET /movies/genres/{Genre}
-		/// </summary>
-		public object Get(Movies request)
-		{
-			return new MoviesResponse
-				       {
-					       Movies = request.Genre.IsNullOrEmpty()
-						                ? Db.Select<Movie>()
-						                : Db.Select<Movie>("Genres LIKE {0}", "%{0}%".Fmt(request.Genre))
-				       };
-		}
-	}
+            return new HttpResult
+            {
+                StatusCode = HttpStatusCode.NoContent,
+                Headers = {
+					{HttpHeaders.Location, this.Request.AbsoluteUri.CombineWith(request.Id.ToString())}
+				}
+            };
+        }
+    }
 }
